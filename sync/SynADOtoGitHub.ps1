80ee2f673e4b07361771070a4419ca0121f8e755
Jessie Sanchez <jessie@jsan.codes>
2025-03-18 22:28:51 -0700

    Update SynADOtoGitHub.ps1

--- sync/SynADOtoGitHub.ps1
+++ sync/SynADOtoGitHub.ps1
@@ -20,7 +20,7 @@ Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
 Write-Host ' reflect Azure Devops repo changes to GitHub repo'
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
 #$AzureRepoName = "CloudQuickLabsADO"
-#$ADOCloneURL = "dev.azure.com/CloudQuickLabs/CloudQuickLabsADO/_git/CloudQuickLabsADO"
+#$ADOCloneURL = "dev.azure.com/nerdlabio/nerdlab.io/_git/avd-accelerator"
 #$GitHubCloneURL = "github.com/RekhuGopal/CloudQuickLabsADO.git"
 $stageDir = pwd | Split-Path
 Write-Host "stage Dir is : $stageDir"
